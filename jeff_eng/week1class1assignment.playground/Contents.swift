09bd76ac758897f3ffb4582338a117b2a28f4d2d
Jeff Eng <jeff@jeffeng.me>
2016-06-06 14:24:09 -0700

    wrote the string character counter function. tested it and functions as expected

---
+++ jeff_eng/week1class1assignment.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Write a function that takes in a string, checks if it can be converted to a number and if possible, returns a number
+//func numConversionChecker(someString: String) -> Int {
+//    
+//}
+
+
+
+//Write a function that takes in a tuple (code, message) and returns a string by using interpolation
+
+
+
+
+
+//Write a function that take in a string, and returns a message: “String contains n characters”
+func stringCharacterCount(string: String) -> String {
+    let numOfCharacters = string.characters.count
+    return "String contains \(numOfCharacters) characters"
+}
+
+stringCharacterCount("Hello")
+
+
+
+//Write a function that takes in a string and reverses it. Use a simple for loop
+func stringReverser(string: String) {
+    
+}
+
+
+
+//Bonus: Declare a variable that’s function. Implement function to take in a string (name) and return greeting message.
+
+
